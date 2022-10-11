f23283c635d6edea510018e74762d905d38a67f9
lulu <318266741@qq.com>
2022-10-12 00:03:21 +0800

    再次修改jdbc
    哈哈哈不不不

--- servle/src/main/java/com/xing/controller/JDBC.java
+++ servle/src/main/java/com/xing/controller/JDBC.java
@@ -5,5 +5,7 @@ public class JDBC {
 
     public static void main(String[] args) {
         System.out.println("55555");
+        System.out.println("55555");
+        System.out.println("55555");
     }
 }

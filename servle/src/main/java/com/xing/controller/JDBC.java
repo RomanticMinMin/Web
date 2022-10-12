26527ca9d0e938672fbc593546c6527ae18ea360
lulu <318266741@qq.com>
2022-10-12 21:58:27 +0800

    xiu改jdbc类

--- servle/src/main/java/com/xing/controller/JDBC.java
+++ servle/src/main/java/com/xing/controller/JDBC.java
@@ -6,6 +6,8 @@ public class JDBC {
     public static void main(String[] args) {
         System.out.println("55555");
         System.out.println("55555");
-        System.out.println("55555");
+        System.out.println("55533355");
+        System.out.println("555444455");
+        System.out.println("511111111");
     }
 }

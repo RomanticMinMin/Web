1b6d7f95d6e619020c6fb563132710101afece9a
lulu <318266741@qq.com>
2022-10-13 11:33:43 +0800

    实现了

--- servle/src/main/java/com/xing/controller/Hello.java
+++ servle/src/main/java/com/xing/controller/Hello.java
@@ -9,5 +9,13 @@ public class Hello {
         System.out.println("dfffffffff");
 
         System.out.println("fffsssssss");
+        int sum=0;
+
+        for (int i =1 ;i < 6;i++){
+            if (i%2!=0){
+                sum +=i;
+            }
+        }
+        System.out.println(sum);
     }
 }
